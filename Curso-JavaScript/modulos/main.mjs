3aedda441a34e4fe51436a35c4800931c7467ac8
MachadiDev <machadianhanguera@gmail.com>
2022-07-04 23:05:21 -0300

    Aulas sobre modulos e orientação a objetos em javascript

---
+++ Curso-JavaScript/modulos/main.mjs
@@ -0,0 +1,7 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade("Gabriel", 24));
+
+console.log(mostraCidade("Gabriel", "Brotas"));
+
+console.log(mostraHobby("Gabriel", "Jogar video games"));
